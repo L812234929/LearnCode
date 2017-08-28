8d42fdd56709a981e26fd0da48c6fabb919503f6
L812234929 <812234929@qq.com>
2017-08-28 14:58:27 +0800

    create
    create

---
+++ public/CharacterCode.cpp
@@ -0,0 +1,34 @@
+#include "CharacterCode.h"
+#include <string.h>
+
+#ifndef NULL
+#define NULL ((void *)0)
+#endif
+
+void FullToHalf(char* pStr)
+{
+	char *pDest	= pStr;
+	if (NULL == pStr)
+		return;
+	for (; *pStr; ++pStr)
+	{
+		if (((*pStr) & 0xFF) == 0xA3 && (*(pStr + 1) & 0xFF) >= 0xA1 && (*(pStr + 1) & 0xFF) <= 0xFE)
+		{
+			// ASCII码中其它可显示字符
+			*pDest++	= (char)((unsigned char)(*(++pStr)) - 0x80);
+			continue;   
+		}
+		else if ((*pStr & 0xFF == 0xA1) && (*(pStr+1) & 0xFF) == 0xA1)
+		{
+			// 全角空格
+			*pDest++	= 0x20;
+		}
+		else
+ 		{
+			if (*pStr < 0)
+				*pDest++	= *(pStr++);
+			*pDest++	= *pStr;
+		}
+	}
+	*pDest	= '\0';
+}
---
+++ public/CharacterCode.h
@@ -0,0 +1,8 @@
+#ifndef _CHARACTER_CODE_HELPER_H_
+#define _CHARACTER_CODE_HELPER_H_
+
+
+// 将字符串中的全角字符转成半角字符
+void FullToHalf(char* pStr);
+
+#endif // _CHARACTER_CODE_HELPER_H_
